aa1403e6f25254b2e08065064136ab1692d1b3bb
Ch.Eswar <150889483+Eswarchinthakayala-webdesign@users.noreply.github.com>
2025-03-05 18:59:19 +0530

    Create program.java

---
+++ Day-1/Patterns/Pattern10/program.java
@@ -0,0 +1,23 @@
+package com.sriram.OOP3;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+      pattern(5);
+    }
+
+    static void pattern(int n)
+    {
+        n=2*n;
+        for (int i = 0; i <=n ; i++) {
+            for (int j = 0; j <=n; j++) {
+                int left=j;
+                int right=n-j;
+                int top=i;
+                int down=n-i;
+                int val=n/2-Math.min(left,Math.min(right,Math.min(top,down)));
+                System.out.print(val);
+            }
+            System.out.println();
+        }
+    }
+}
