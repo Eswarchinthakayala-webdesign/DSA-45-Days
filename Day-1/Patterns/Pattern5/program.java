94fd07bdacd566a5acc652314773130dd9db5b58
Ch.Eswar <150889483+Eswarchinthakayala-webdesign@users.noreply.github.com>
2025-03-05 17:58:55 +0530

    Update program.java

--- Day-1/Patterns/Pattern5/program.java
+++ Day-1/Patterns/Pattern5/program.java
@@ -8,17 +8,11 @@ public class Main {
     static void pattern(int n)
     {
         for (int i = 1; i <=2*n-1; i++) {
-            if(i<=n) {
-                for (int j = 1; j <=i; j++) {
+            int stars=Math.min(i,2*n-i);
+                for (int j = 1; j <=stars; j++) {
                     System.out.print("* ");
                 }
-            }
-            else
-            {
-                for (int j = 1; j <=n-(i%n); j++) {
-                    System.out.print("* ");
-                }
-            }
+
             System.out.println();
         }
     }
